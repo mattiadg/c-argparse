8a83560f2a8cd398150015fc42e8a099d35f36bc
Mattia Di Gangi <mattiadg@users.noreply.github.com>
2022-09-09 01:15:50 +0200

    Add initial scaffolding for tests

---
+++ test/test_parser.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "test_parser.h"
+#include "../src/argument_parser.h"
+
+int all_tests() {
+    _verify(test_init_parser);
+    return 0;
+}
+
+int test_init_parser(){
+    Parser parser = init_parser("a name");
+    _assert(!strncmp(parser.name, "a name", 7));
+    _assert(parser.positional_args == NULL);
+    _assert(parser.optional_args == NULL);
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    int r = all_tests();
+    if (r == 0) {
+        printf("PASSED");
+    }
+    return 0;
+}
