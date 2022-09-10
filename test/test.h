6f5b140f1a0925e57f8a6afb057c0c0d9ea02f9f
Mattia Di Gangi <mattiadg@users.noreply.github.com>
2022-09-10 13:01:28 +0200

    Add reference to code source and add include guard

--- test/test.h
+++ test/test.h
@@ -1,6 +1,15 @@
+#ifndef TEST_H
+#define TEST_H 
+
 #include <stdio.h>
 
+/**
+ * @brief Code for automating unit tests from 
+ * https://eradman.com/posts/tdd-in-c.html
+ * 
+ */
 #define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
 #define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
 #define _verify(test) do { int r=test(); tests_run++; if(r) return r; } while(0)
 
+#endif
